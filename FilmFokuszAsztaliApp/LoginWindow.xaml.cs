d83e30074045d5e2f21d0d0ae0ba8c80c684691a
Miklos Merse <miklosm@kkszki.hu>
2025-02-04 11:26:36 +0100

    WPF

---
+++ FilmFokuszAsztaliApp/LoginWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using FilmFokuszBackEnd.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FilmFokuszAsztaliApp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public HttpClient client;
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var response = await client.PostAsync($"api/Login/SaltRequest/{tbxLoginName.Text}", new StringContent(tbxLoginName.Text, Encoding.UTF8, "text/plain"));
+            string salt = await response.Content.ReadAsStringAsync();
+            MessageBox.Show(salt);
+            string tmpHash = MainWindow.CreateSHA256(tbxPassword.Password + salt);
+            MessageBox.Show(tmpHash);
+            LoginDTO dtoUser = new LoginDTO()
+            {
+                LoginName = tbxLoginName.Text,
+                TmpHash = tmpHash
+            };
+
+            string felhAdatok = JsonSerializer.Serialize(dtoUser, JsonSerializerOptions.Default);
+            var body = new StringContent(felhAdatok, Encoding.UTF8, "application/json");
+            var valasz = await client.PostAsync("api/Login/", body);
+            var content = await valasz.Content.ReadAsStringAsync();
+            MessageBox.Show(content);
+
+            string[] darabok = content.Split('"');
+            string tokenem = darabok[darabok.Length - 2];
+            MessageBox.Show(tokenem);
+            MainWindow.uId = tokenem;
+            Close();
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
