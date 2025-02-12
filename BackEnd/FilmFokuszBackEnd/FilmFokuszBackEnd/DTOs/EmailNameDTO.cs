08e0a1dfbbe41745e1b76fb64f7e5bcedb26efca
MklsMerse <145961860+MklsMerse@users.noreply.github.com>
2025-02-12 10:21:28 +0100

    Delete BackEnd/FilmFokuszBackEnd/FilmFokuszBackEnd directory

--- BackEnd/FilmFokuszBackEnd/FilmFokuszBackEnd/DTOs/EmailNameDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FilmFokuszBackEnd.DTOs
-{
-    public class EmailNameDTO
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-    }
-}
--- BackEnd/FilmFokuszBackEnd/FilmFokuszBackEnd/DTOs/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FilmFokuszBackEnd.DTOs
-{
-    public class LoginDTO
-    {
-        public string LoginName { get; set; }
-
-        public string TmpHash { get; set; }
-    }
-}
